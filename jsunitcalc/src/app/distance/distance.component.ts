6baa668f251f8c2d4484b31ee2aaaeef9bb6856a
Tyler Tucker <webcooltz@duck.com>
2021-12-26 22:51:32 -0700

    added files

---
+++ jsunitcalc/src/app/distance/distance.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-distance',
+  templateUrl: './distance.component.html',
+  styleUrls: ['./distance.component.css']
+})
+export class DistanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  miles = 1;
+
+  kilometers = 1.61;
+
+}
---
+++ jsunitcalc/src/app/temperature/temperature.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temperature',
+  templateUrl: './temperature.component.html',
+  styleUrls: ['./temperature.component.css']
+})
+export class TemperatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  fahrenheit = 32;
+  celsius = 0;
+
+}
