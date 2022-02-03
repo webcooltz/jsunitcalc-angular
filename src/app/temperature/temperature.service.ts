0335b4926532c842f53566bfe686a21b67f394cf
Tyler Tucker <webcooltz@duck.com>
2022-02-02 19:58:19 -0700

    adding services

---
+++ src/app/temperature/temperature.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+// probably not necessary since it isn't re-used
+
+@Injectable({
+  providedIn:'root'
+})
+export class TemperatureService {
+
+  fahrenheitToCelsius() {
+
+  }
+
+}
